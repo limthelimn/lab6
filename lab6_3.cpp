d195bda1d39f1d3f7e454fe0cba62d4c6c69891e
limthelimn <nuttawan.lim@gmail.com>
2020-12-25 17:16:22 +0700

    lab6/3

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -1,12 +1,19 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
+char before(char X){
+	if(X == 65){
+        return X = 'Z';
+    }
+    if(X >= 65 && X <=90){
+        X = X-1;
+    }else{
+        return '0';
+    }
+    return X;
 }
-
 int main(){
-	//Test Case
+	
 	cout << before('A') << "\n";
 	cout << before('B') << "\n";
 	cout << before('P') << "\n";
